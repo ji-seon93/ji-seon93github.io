b802f93108614aba248812851573372b9137af88
ji-seon93 <62415959+ji-seon93@users.noreply.github.com>
2020-05-08 14:12:56 +0900

    Add files via upload

---
+++ template.js
@@ -0,0 +1,156 @@
+/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
+ * Author:HtmlCoder
+ * Author URI:http://www.htmlcoder.me
+ * Version:1.0.0
+ * Created:November 2014
+ * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
+ * File Description: Initializations of plugins 
+ */
+
+(function($){
+	$(document).ready(function(){
+	
+		$(".banner-image").backstretch('images/a2.png');
+		
+		// Fixed header
+		//-----------------------------------------------
+		$(window).scroll(function() {
+			if (($(".header.fixed").length > 0)) { 
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+					$("body").addClass("fixed-header-on");
+				} else {
+					$("body").removeClass("fixed-header-on");
+				}
+			};
+		});
+
+		$(window).load(function() {
+			if (($(".header.fixed").length > 0)) { 
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+					$("body").addClass("fixed-header-on");
+				} else {
+					$("body").removeClass("fixed-header-on");
+				}
+			};
+		});
+
+		//Scroll Spy
+		//-----------------------------------------------
+		if($(".scrollspy").length>0) {
+			$("body").addClass("scroll-spy");
+			$('body').scrollspy({ 
+				target: '.scrollspy',
+				offset: 152
+			});
+		}
+
+		//Smooth Scroll
+		//-----------------------------------------------
+		if ($(".smooth-scroll").length>0) {
+			$('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function() {
+				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+					var target = $(this.hash);
+					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+					if (target.length) {
+						$('html,body').animate({
+							scrollTop: target.offset().top-151
+						}, 1000);
+						return false;
+					}
+				}
+			});
+		}
+
+		// Animations
+		//-----------------------------------------------
+		if (($("[data-animation-effect]").length>0) && !Modernizr.touch) {
+			$("[data-animation-effect]").each(function() {
+				var $this = $(this),
+				animationEffect = $this.attr("data-animation-effect");
+				if(Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
+					$this.appear(function() {
+						setTimeout(function() {
+							$this.addClass('animated object-visible ' + animationEffect);
+						}, 400);
+					}, {accX: 0, accY: -130});
+				} else {
+					$this.addClass('object-visible');
+				}
+			});
+		};
+
+		// Isotope filters
+		//-----------------------------------------------
+		if ($('.isotope-container').length>0) {
+			$(window).load(function() {
+				$('.isotope-container').fadeIn();
+				var $container = $('.isotope-container').isotope({
+					itemSelector: '.isotope-item',
+					layoutMode: 'masonry',
+					transitionDuration: '0.6s',
+					filter: "*"
+				});
+				// filter items on button click
+				$('.filters').on( 'click', 'ul.nav li a', function() {
+					var filterValue = $(this).attr('data-filter');
+					$(".filters").find("li.active").removeClass("active");
+					$(this).parent().addClass("active");
+					$container.isotope({ filter: filterValue });
+					return false;
+				});
+			});
+		};
+
+		//Modal
+		//-----------------------------------------------
+		if($(".modal").length>0) {
+			$(".modal").each(function() {
+				$(".modal").prependTo( "body" );
+			});
+		}
+
+	}); // End document ready
+})(this.jQuery);
+
+$(function () {
+
+    activateScene2();
+
+    // Scene 2를 표시
+    function activateScene2 () {
+
+        var $content = $('#scene-2-content'),
+            $charts = $content.find('.chart');
+
+        // 원형 차트 당 처리
+        $charts.each(function(){
+            var $chart = $(this),
+                // "마스크"를 저장하고 각도 0으로 지정
+                $circleLeft = $chart.find('.left .circle-mask-inner')
+                    .css({ transform: 'rotate(0)' }),
+                $circleRight = $chart.find('.right .circle-mask-inner')
+                    .css({ transform: 'rotate(0)' }),
+                // 백분율 값을 취득
+                $percentNumber = $chart.find('.percent-number'),
+                percentData = $percentNumber.text();
+
+            // 백분율 표시를 일단 0
+            $percentNumber.text(0);
+
+            // 각도 애니메이션
+            $({ percent: 0 }).delay(1000).animate({ percent: percentData }, {
+                duration: 1500, 
+                progress: function () {
+                    var now = this.percent,
+                        deg = now * 360 / 100,
+                        degRight = Math.min(Math.max(deg, 0), 180),
+                        degLeft  = Math.min(Math.max(deg - 180, 0), 180);
+                    $circleRight.css({ transform: 'rotate(' + degRight + 'deg)' });
+                    $circleLeft.css({ transform: 'rotate(' + degLeft + 'deg)' });
+                    $percentNumber.text(Math.floor(now));
+                }
+            });
+        });
+    }
+
+});
